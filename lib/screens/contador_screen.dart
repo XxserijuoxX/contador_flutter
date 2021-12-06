bc3bc7b7eddc7c582e760dfc091ab696a3e9fa1f
NReyes <sergio_one_x@hotmail.com>
2021-12-05 23:42:30 -0700

    Update contador_screen.dart

--- lib/screens/contador_screen.dart
+++ lib/screens/contador_screen.dart
@@ -1,5 +1,3 @@
-import 'package:flutter/material.dart';
-
 class ContadorScreen extends StatefulWidget {
   //const HomeScreen({Key key}) : super(key: key);
   @override
@@ -10,21 +8,23 @@ class _ContadorScreenState extends State<ContadorScreen> {
   int contador = 0;
 
   @override
+  
+  
+  
   Widget build(BuildContext context) {
+    TextStyle fontSize20 = const TextStyle (fontSize: 20);
+    
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Pantalla Contador")),
+        title: const Center(child: Text("Pantalla Contador")),
         elevation: 10,
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Contador de clics', style: TextStyle(fontSize: 20)),
-            Text(
-              '$contador',
-              style: TextStyle(fontSize: 20),
-            )
+            Text('Contador de clics', style: fontSize20),
+            Text( '$contador', style: fontSize20),
           ],
         ),
       ),
@@ -33,21 +33,21 @@ class _ContadorScreenState extends State<ContadorScreen> {
         children: [
           FloatingActionButton(
             onPressed: () => setState(() => contador++),
-            child: Icon(
+            child: const Icon(
               Icons.add,
               size: 30,
             ),
           ),
           FloatingActionButton(
             onPressed: () => setState(() => contador = 0),
-            child: Icon(
+            child: const Icon(
               Icons.undo,
               size: 30,
             ),
           ),
           FloatingActionButton(
             onPressed: () => setState(() => contador--),
-            child: Icon(
+            child: const Icon(
               Icons.remove,
               size: 30,
             ),
